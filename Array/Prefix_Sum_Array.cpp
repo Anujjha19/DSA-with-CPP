daf3b6256c2d383f45690c08dbd3a024155e809e
Anuj Jha <anujjha710@gmail.com>
2023-01-28 13:36:50 +0530

    Subarray

--- Array/Prefix_Sum_Array.cpp
+++ Array/Prefix_Sum_Array.cpp
@@ -3,20 +3,29 @@ using namespace std;
 
 int main()
 {
-    int arr[] = { 1,2,3,4 };
+    int arr[] = {1, 2, 3, 4};
     int n = sizeof(arr) / sizeof(arr[0]);
-    int pre[n];
+    /* int pre[n-1];
     pre[0]=arr[0];
     for(int i=1 ; i < n; i++){
         pre[i] = pre[i-1] + arr[i];
     }
+
     for(int i=0; i< n; i++){
         cout <<  pre[i] << " " ;
+    } */
+
+    int sum = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        sum += arr[i];
+        arr[i] = sum;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
     }
 
-    
-
     return 0;
-
-
 }
