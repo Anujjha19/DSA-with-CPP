076ff5a7f5b0295a16f72c2e81c91ee6a64e4359
Anuj Jha <anujjha710@gmail.com>
2023-02-21 10:28:21 +0530

    Sorting

---
+++ Sorting/Quick_Sort.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int partitionQS(int arr[], int s, int e)
+{
+    int pivot = arr[s];
+
+    int count = 0;
+
+    for (int i = s + 1; i <= e; i++)
+    {
+        if (arr[i] <= pivot)
+        {
+            count++;
+        }
+    }
+    // placed pivot at right place
+    int pivotIndex = s + count;
+
+    swap(arr[pivotIndex], arr[s]);
+
+    int i = s;
+    int j = e;
+
+    while (i < pivotIndex && j > pivotIndex)
+    {
+
+        while (arr[i] <= pivot)
+        {
+            i++;
+        }
+        while (arr[j] >= pivot)
+        {
+            j--;
+        }
+        if (i < pivotIndex && j > pivotIndex)
+        {
+            swap(arr[i], arr[j]);
+            i++;
+            j--;
+        }
+    }
+    return pivotIndex;
+}
+
+void quickSort(int arr[], int n, int s, int e)
+{
+
+    if (s >= e)
+        return;
+
+    int p = partitionQS(arr, s, e);
+
+    quickSort(arr, n, s, p - 1);
+
+    quickSort(arr, n, p + 1, e);
+}
+
+int main()
+{
+    int arr[5] = {4, 100, 55, 0, 1};
+    int n = 5;
+
+    quickSort(arr, n, 0, n - 1);
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
