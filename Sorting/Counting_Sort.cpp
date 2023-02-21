c0dbf0e8337293a5367b7def2406c41213540234
Anuj Jha <anujjha710@gmail.com>
2023-02-21 09:05:31 +0530

    Sorting

--- Sorting/Counting_Sort.cpp
+++ Sorting/Counting_Sort.cpp
@@ -2,19 +2,24 @@
 using namespace std;
 
 void countSort(int arr[], int n, int k) {
+    //Max Size Array
     int count[k+1];
     for(int i=0; i<=k; i++) {
         count[i] = 0;
     }
+    //Count each Element
     for(int i=0; i<n; i++) {
         count[arr[i]]++;
     }
+    //Position - add before + current
     for(int i=1; i<=k; i++) {
         count[i] += count[i-1];
     }
+    //Temp Array
+    
     int output[n];
     for(int i=n-1; i>=0; i--) {
-        output[count[arr[i]]-1] = arr[i];
+        output[count[arr[i]]  -  1 ] = arr[i];
         count[arr[i]]--;
     }
     for(int i=0; i<n; i++) {
@@ -31,6 +36,7 @@ int main() {
     for(int i=0; i<n; i++) {
         cin >> arr[i];
     }
+    //Max Element
     int k = arr[0];
     for(int i=1; i<n; i++) {
         if(arr[i] > k) {
