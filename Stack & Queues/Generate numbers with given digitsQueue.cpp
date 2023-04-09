fed1711b666cb27350caac479f5464d222111f7f
Anuj Jha <anujjha710@gmail.com>
2023-04-09 21:16:25 +0530

    Queue

---
+++ Stack & Queues/Generate numbers with given digitsQueue.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h> 
+#include <queue>
+using namespace std; 
+
+
+void printFirstN(int n) 
+{ 
+    queue<string> q;
+    
+    q.push("5");
+    q.push("6");
+    
+    for(int i = 0; i < n; i++)
+    {
+        string curr = q.front();
+       
+        cout << curr << " ";
+        
+        q.pop();
+        
+        q.push(curr + "5");
+        q.push(curr + "6");
+    }
+        
+} 
