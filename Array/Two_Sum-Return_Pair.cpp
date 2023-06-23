a941a73a7870f5827c6e90d7e4264d97356c6c4a
Anuj Jha <anujjha710@gmail.com>
2023-06-23 23:20:52 +0530

    Arrays

--- Array/Two_Sum-Return_Pair.cpp
+++ Array/Two_Sum-Return_Pair.cpp
@@ -1,33 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> twoSum1(int n, vector<int> &arr, int target) {
+vector<int> twoSum1(int n, vector<int> &arr, int target)
+{
     vector<int> ans;
-    for (int i = 0; i < n; i++) {
-        for (int j = i + 1; j < n; j++) {
-            if (arr[i] + arr[j] == target) {
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[i] + arr[j] == target)
+            {
                 ans.push_back(i);
                 ans.push_back(j);
                 return ans;
             }
         }
     }
-    return { -1, -1};
+    return {-1, -1};
 }
 
-vector<int> twoSum(int n, vector<int> &arr, int target) {
+vector<int> twoSum(int n, vector<int> &arr, int target)
+{
     unordered_map<int, int> mpp;
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         int num = arr[i];
         int moreNeeded = target - num;
-        if (mpp.find(moreNeeded) != mpp.end()) {
+        if (mpp.find(moreNeeded) != mpp.end())
+        {
             return {mpp[moreNeeded], i};
         }
         mpp[num] = i;
     }
-    return { -1, -1};
+    return {-1, -1};
 }
-
 
 int main()
 {
@@ -39,4 +45,3 @@ int main()
          << ans[1] << "]" << endl;
     return 0;
 }
- 
