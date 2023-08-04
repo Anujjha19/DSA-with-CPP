796d77de13f0acc7590edfa46d7b63a2eba93f81
Anuj Jha <anujjha710@gmail.com>
2023-08-04 08:37:00 +0530

    Sorting

--- Sorting/Quick_Sort.cpp
+++ Sorting/Quick_Sort.cpp
@@ -43,6 +43,26 @@ int partitionQS(int arr[], int s, int e)
     return pivotIndex;
 }
 
+/* int partition(vector<int> &arr, int low, int high) {
+    int pivot = arr[low];
+    int i = low;
+    int j = high;
+
+    while (i < j) {
+        while (arr[i] <= pivot && i <= high - 1) {
+            i++;
+        }
+
+        while (arr[j] > pivot && j >= low + 1) {
+            j--;
+        }
+        if (i < j) swap(arr[i], arr[j]);
+    }
+    swap(arr[low], arr[j]);
+    return j;
+}
+ */
+
 void quickSort(int arr[], int n, int s, int e)
 {
 
